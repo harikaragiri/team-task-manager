e0ef1b7d5531df0b9b1dc5e7909797ca132a2fac
Harika <harika_ragiri@srmap.edu.in>
2026-05-02 12:16:02 +0530

    Updated code

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://team-task-manager-ngc2.onrender.com',
+  baseURL: process.env.REACT_APP_API_URL || 'https://team-task-manager-ngc2.onrender.com/api',
 });
 
 API.interceptors.request.use((config) => {
